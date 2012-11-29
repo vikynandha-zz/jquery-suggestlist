d5e57b1f12ac674cda43c0b6074f2be5a2a6bd9d
Vignesh Nandha Kumar <viky.nandha@gmail.com>
2012-11-29 22:54:36 +0530

    Basic version in working condition

---
+++ js/duration-picker.js
@@ -0,0 +1,174 @@
+( function( $ ) {
+
+	function DurationPicker( element, options ) {
+		var that = this;
+		this.$element = $( element ).attr( 'autocomplete', 'off' );
+		this.options = $.extend( $.fn.durationpicker.defaults, options );
+		this.picker = DPGlobal.render( options ).on( 'click.durationpicker', 'li', $.proxy( this.click, this ) );
+		this.isInput = this.$element.is( 'input' );
+
+		this.picker.width( this.$element.outerWidth() );
+		$( document ).on( 'mousedown', function ( event ) {
+			// Clicked outside the datepicker, hide it
+			if ( $( event.target ).closest( '.durationpicker' ).length == 0 ) {
+				that.hide();
+			}
+		});
+		if ( this.isInput ) {
+			this.$element.on( {
+				focus: $.proxy( this.show, this ),
+				keydown: $.proxy( this.keydown, this )
+			} );
+		}
+	}
+
+	DurationPicker.prototype = {
+		constructor: DurationPicker,
+
+        click: function( event ) {
+            this.picker.find( 'li.durationpicker-selected' ).removeClass( 'durationpicker-selected' );
+            $( event.currentTarget ).addClass( 'durationpicker-selected' );
+            this.update();
+        },
+
+		keydown: function( event ) {
+			switch( event.keyCode ) {
+			case 27: // ESC
+				this.hide();
+				event.preventDefault();
+				break;
+			case 9: // Tab
+				this.hide();
+				break;
+			case 38: // Up
+				if ( this.picker.is( ':not(:visible)' ) ) {
+					this.show();
+					return;
+				}
+				this.selectPrev();
+				break;
+			case 40: // Down
+				if ( this.picker.is( ':not(:visible)' ) ) {
+					this.show();
+					return;
+				}
+				this.selectNext();
+				break;
+			case 13: // Enter
+				this.update( event );
+				event.preventDefault();
+				break;
+			}
+		},
+
+		show: function( event ) {
+			this.picker.show();
+			this.place();
+			$( window ).on( 'resize', $.proxy( this.place, this ) );
+			if ( event ) {
+				event.stopPropagation();
+				event.preventDefault();
+			}
+		},
+
+		hide: function( event ) {
+			this.picker.hide();
+			$( window ).off( 'resize', $.proxy( this.place, this ) );
+		},
+
+		place: function() {
+			var offset = this.$element.offset();
+			this.picker.css( {
+				left: offset.left,
+				top: offset.top + this.$element.outerHeight()
+			} );
+		},
+
+		selectNext: function() {
+			var $selected, $target;
+			$selected = this.picker.find( 'li.durationpicker-selected' ).first();
+
+			if ( $selected.length === 0 ) {
+				$target = this.picker.find( 'li' ).first();
+			} else {
+				if ( $selected.is( ':last-child' ) ) {
+					return;
+				}
+				$target = $selected.removeClass( 'durationpicker-selected' ).next();
+			}
+			$target.addClass( 'durationpicker-selected' );
+		},
+
+		selectPrev: function() {
+			var $selected, $target;
+			$selected = this.picker.find( 'li.durationpicker-selected' ).first();
+
+			if ( $selected.length === 0 ) {
+				$target = this.picker.find( 'li' ).last();
+			} else {
+				if ( $selected.is( ':first-child' ) ) {
+					return;
+				}
+				$target = $selected.removeClass( 'durationpicker-selected' ).prev();
+			}
+			$target.addClass( 'durationpicker-selected' );
+		},
+
+		update: function( event ) {
+			this.$element.val( this.picker.find( 'li.durationpicker-selected' ).text() );
+			this.hide();
+		}
+
+	};
+
+	var DPGlobal ={
+		render: function( options ) {
+			var $list = $( '<ul/>' ), i;
+			$list.addClass( 'durationpicker' )
+				.css( {
+					zIndex: getClosestZIndex( this.$element ),
+				} );
+
+			for( i = 0; i < options.list.length; i++ ) {
+				$list.append( '<li>' + options.list[i] + '</li>' );
+				if ( i === 0 ) {
+					$list.addClass( 'durationpicker-selected' );
+				}
+			}
+			$list.appendTo( 'body' );
+			return $list;
+		}
+	};
+
+	/* Helper functions */
+	function getClosestZIndex( elem ) {
+		if ( !elem ) return;
+		return parseInt( elem.parents().filter( function() {
+			return $( this ).css( 'z-index' ) !== 'auto';
+		} ).first().css( 'z-index' ), 10 );
+	};
+
+
+	/* Add durationpicker to jQuery */
+	$.fn.durationpicker = function( method ) {
+		var args = Array.apply(null, arguments);
+		args.shift();
+		return this.each( function () {
+			var $this = $( this ),
+				data = $this.data( 'durationpicker' ),
+				options = typeof option == 'object' && option;
+			if ( !data ) {
+				data = new DurationPicker( this, $.extend({}, $.fn.durationpicker.defaults, options ) );
+				$this.data( 'durationpicker', data);
+			}
+			if (typeof option == 'string' && typeof data[option] == 'function') {
+				data[option].apply(data, args);
+			}
+		});
+	}
+
+	$.fn.durationpicker.defaults = {
+		list: ['15 min', '30 min', '1 hr', '2 hr']
+	};
+
+} ) ( jQuery );
